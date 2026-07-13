51b149544c68c113dbc2fd9bb92a654c5b6f8423
suraj <surajnakhale2407@gmail.com>
2026-07-13 18:05:03 +0530

    setup react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div className="h-screen w-screen">
+        <nav className="h-auto w-full flex justify-between items-center">
+          <div>trelloop</div>
+          <div className="h-auto w-auto flex gap-8">
+            <div>about</div>
+            <div>features</div>
+            <div>get started</div>
+          </div>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
